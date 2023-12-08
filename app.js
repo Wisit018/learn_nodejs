5635a08673884e5b6ad816769ba8f7918cff2fff
Wisit018 <lipton_pink@hotmail.co.th>
2023-12-08 16:52:33 +0700

    lecture 19 :100:
    lecture 19 :100:

--- app.js
+++ app.js
@@ -5,19 +5,40 @@ const chalk = require("chalk"); // ใส่สี
 const debug = require('debug')('app'); // ดูเหตุการ์ณว่ามีอะไรบ้าง
 const morgan = require('morgan'); // ดู Log
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views")
+app.set("view engine", "ejs")
+
+productRouter.route("/").get((req,res) =>{
+    res.render('products',{
+        products: [
+            {produtsTitle: 'item1' ,Descripttion: 'ID : 1' , Price : '100'},
+            {produtsTitle: 'item2' ,Descripttion: 'ID : 2' , Price : '200'},
+            {produtsTitle: 'item3' ,Descripttion: 'ID : 3' , Price : '300'},
+            {produtsTitle: 'item4' ,Descripttion: 'ID : 4' , Price : '400'}
+        ]
+    })
+})
+
+productRouter.route("/1").get((req,res) =>{
+    res.send("Hello Products");
+})
+
+app.use("/products", productRouter)
+
 app.get("/",(req,res) =>{
 
-    res.send("hello World ");
+    res.render('index',{username: 'Wisit Phusakul', customers: ["A1","A2","A3","A4"]});
 
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port" + chalk.green(port));
+app.listen(PORT, ()=>{
+    debug("Listening on PORT" + chalk.green(PORT));
 })
