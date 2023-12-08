022994e1d0b78efed500fb98cc25221b8d460b8a
Wisit018 <lipton_pink@hotmail.co.th>
2023-12-08 12:02:30 +0700

    demo test

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require('express')
 const chalk = require("chalk"); // ใส่สี
 const debug = require('debug')('app'); // ดูเหตุการ์ณว่ามีอะไรบ้าง
 const morgan = require('morgan'); // ดู Log
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/",(req,res) =>{
 
