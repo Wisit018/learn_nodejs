872c6ed97ab69a1364e8c315aec7947f80bf0faa
Wisit018 <lipton_pink@hotmail.co.th>
2023-12-09 10:36:52 +0700

    update to mongoDB
    :+1:

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const chalk = require("chalk"); // ใส่สี
 const debug = require('debug')('app'); // ดูเหตุการ์ณว่ามีอะไรบ้าง
 const morgan = require('morgan'); // ดู Log
 const path = require('path');
-const products = require("./data/products.json")
-const productRouter = express.Router();
+
 
 const app = express();
 const PORT = process.env.PORT || 4000
+const productsRouter =require("./src/router/productsRouter")
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
@@ -17,18 +17,8 @@ app.use(express.static(path.join(__dirname,"/public/")));
 app.set("views","./src/views")
 app.set("view engine", "ejs")
 
-productRouter.route("/").get((req,res) =>{
-    res.render('products',
-        products,
-    )
-})
 
-productRouter.route("/:id").get((req,res) =>{
-    const id = req.params.id;
-    res.send("Hello Products");
-})
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/",(req,res) =>{
 
